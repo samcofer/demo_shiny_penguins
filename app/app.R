c3bbddce4d4afc60c65b32f98372590dd744a325
SamEdwardes <edwardes.s@gmail.com>
2022-03-28 13:06:55 -0700

    Added island dropdown

--- app/app.R
+++ app/app.R
@@ -3,14 +3,22 @@ library(shinydashboard)
 library(ggplot2)
 library(dplyr)
 
-
+# Load the data
+penguins <- palmerpenguins::penguins
 
 body <- shinydashboard::dashboardBody(
     fluidRow(
         column(
-            6, 
+            6,
             tags$h4("Meeting the penguins!"),
-            tags$p("Data were collected and made available by Dr. Kristen Gorman and the Palmer Station, Antarctica LTER, a member of the Long Term Ecological Research Network.")),
+            tags$p("Data were collected and made available by Dr. Kristen Gorman and the Palmer Station, Antarctica LTER, a member of the Long Term Ecological Research Network."),
+            tags$p("The penguins live on three different islands:"),
+            selectInput(
+                "islandInput",
+                label = "Island",
+                choices = c("All", levels(penguins$island))
+            )
+        ),
         column(6, tags$img(src = "https://allisonhorst.github.io/palmerpenguins/reference/figures/lter_penguins.png", width = 250))
     ),
     fluidRow(
@@ -32,34 +40,47 @@ ui <- shinydashboard::dashboardPage(
 
 
 server <- function(input, output) {
-  
-    # Load the data
-    penguins <- palmerpenguins::penguins
+    
+    selected_island <- reactive({input$islandInput})
+    
+    filtered_penguins <- reactive({
+        print(selected_island())
+        if (selected_island() == "All") {
+            penguins
+        } else {
+            penguins |> 
+                filter(island == selected_island())
+        }
+    })
     
     # Plots
-    fig_bill_vs_flipper <- 
-        penguins |> 
-        ggplot(aes(x = flipper_length_mm, y = bill_length_mm, color = species, shape = species)) +
-        geom_point() +
-        labs(
-            title = "Bill length vs. flipper length by species",
-            x = "Flipper length (mm)",
-            y = "Bill length (mm)"
-        )
+    fig_bill_vs_flipper <- reactive({
+        filtered_penguins() |> 
+            ggplot(aes(x = flipper_length_mm, y = bill_length_mm, color = species, shape = species)) +
+            geom_point() +
+            labs(
+                title = "Bill length vs. flipper length by species",
+                x = "Flipper length (mm)",
+                y = "Bill length (mm)"
+            )
+        
+    })
     
-    fig_flipper_dist <-
-        penguins |> 
-        ggplot(aes(x = flipper_length_mm, fill = species)) +
-        geom_histogram(alpha = 0.7, position = "identity") +
-        labs(
-            title = "Distribution of Flipper Length",
-            x = "Flipper length (mm)",
-            y = "Count"
-        )
+    fig_flipper_dist <- reactive({
+        filtered_penguins() |> 
+            ggplot(aes(x = flipper_length_mm, fill = species)) +
+            geom_histogram(alpha = 0.7, position = "identity") +
+            labs(
+                title = "Distribution of Flipper Length",
+                x = "Flipper length (mm)",
+                y = "Count"
+            )
+    })
+        
         
     # Outputs
-    output$figBillVsFlipper <- renderPlot({fig_bill_vs_flipper})
-    output$figFlipperDist <- renderPlot({fig_flipper_dist}) 
+    output$figBillVsFlipper <- renderPlot({fig_bill_vs_flipper()})
+    output$figFlipperDist <- renderPlot({fig_flipper_dist()}) 
 }
 
 # Run the application 
