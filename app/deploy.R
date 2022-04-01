e7e5841d6cdd2b2f9c5919e6a941a4da63e8ae1f
sam.edwardes <edwardes.s@gmail.com>
2022-04-01 13:33:18 +0000

    Correct server again

--- app/deploy.R
+++ app/deploy.R
@@ -8,8 +8,8 @@ rsconnect::deployApp(
 )
 
 
-rsconnect::connectApiUser(account = "sam.edwardes", server = "colorado.rstudio.com", apiKey={{ env.RSCONNECT_APIKEY }})
+rsconnect::connectApiUser(account = "sam.edwardes", server = "https://colorado.rstudio.com", apiKey={{ env.RSCONNECT_APIKEY }})
 
-rsconnect::connectApiUser(account = "xxx", server = "https://colorado.rstudio.com", apiKey = "Z8uMvm13rcEprf8pUgehclbOgjKRsoi5")
+rsconnect::connectApiUser(account = "xxx", server = "https://colorado.rstudio.com", apiKey = "xx")
 
 rsconnect::deployApp(addDir = "app", appFiles = c("app.R"), appName = "demo-palmers-penguins-shiny-github-actions-deploy", appTitle = "Demo - Palmers Penguins [Shiny / GitHub Actions Deploy]", forceUpdate = TRUE)
