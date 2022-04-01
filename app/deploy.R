bd9463b50e33892cb52212385582990a44d900bb
sam.edwardes <edwardes.s@gmail.com>
2022-04-01 13:21:17 +0000

    Sherver name change

--- app/deploy.R
+++ app/deploy.R
@@ -8,5 +8,5 @@ rsconnect::deployApp(
 )
 
 
-rsconnect::connectApiUser(account = "sam.edwardes", server = "https://colorado.rstudio.com/rsc", apiKey={{ env.RSCONNECT_APIKEY }})
+rsconnect::connectApiUser(account = "sam.edwardes", server = "https://colorado.rstudio.com", apiKey={{ env.RSCONNECT_APIKEY }})
 rsconnect::deployApp(addDir = "app", appFiles = c("app.R"), appName = "demo-palmers-penguins-shiny-github-actions-deploy", appTitle = "Demo - Palmers Penguins [Shiny / GitHub Actions Deploy]", forceUpdate = TRUE)
