e0c449eb939d33cf8b7d621bf154f89649cbb9bd
sam.edwardes <edwardes.s@gmail.com>
2022-04-01 21:30:21 +0000

    WIP

--- app/deploy.R
+++ app/deploy.R
@@ -10,7 +10,27 @@ rsconnect::deployApp(
 
 rsconnect::connectApiUser(account = "sam.edwardes", server = "colorado.rstudio.com", apiKey={{ env.RSCONNECT_APIKEY }})
 
+# TEST >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+server_nickname <- "colorado.rstudio.com"
 
-rsconnect::addServer(url = "https://colorado.rstudio.com/rsc/__api__", name = "colorado.rstudio.com")
-rsconnect::connectApiUser(account = "abasssssd", server = "colorado.rstudio.com", apiKey = Sys.getenv("CONNECT_API_KEY"))
-rsconnect::deployApp(addDir = "app", appFiles = c("app.R"), appName = "demo-palmers-penguins-shiny-github-actions-deploy", appTitle = "Demo - Palmers Penguins [Shiny / GitHub Actions Deploy]", forceUpdate = TRUE)
+rsconnect::addServer(
+  url = "https://colorado.rstudio.com/rsc/", 
+  name = server_nickname
+)
+
+rsconnect::connectUser(account = "sam.edwardes", server = server_nickname)
+
+# TEST <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+rsconnect::connectApiUser(
+  apiKey = Sys.getenv("CONNECT_API_KEY"), 
+  server = server_nickname
+)
+
+rsconnect::deployApp(
+  addDir = "app", 
+  appFiles = c("app.R"), 
+  appName = "demo-palmers-penguins-shiny-github-actions-deploy", 
+  appTitle = "Demo - Palmers Penguins [Shiny / GitHub Actions Deploy]", 
+  forceUpdate = TRUE
+)
